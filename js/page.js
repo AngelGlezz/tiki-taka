f9daf52f289100b24fe453e14b53813296e0e03b
Angel <angelglezz.7@gmail.com>
2018-10-26 10:37:09 -0500

    prueba de galería en secciones

--- js/page.js
+++ js/page.js
@@ -22,6 +22,21 @@ $(document).ready(function(){
 		})
 	});
 
+	$(".item_main").click(function(){
+		$('.item_main').css({
+			'border-bottom': '0px'
+		});
+		$('.item_main').find('a').css({
+			'color': '#fff'
+		});
+		$(this).css({
+			'border-bottom': '1px solid #e9c24e'
+		});
+		$(this).find('a').css({
+			'color': '#e9c24e'
+		});
+	});
+
 	$("#fifa").off('mouseleave');
 	$("#fifa").off('mouseenter');
 	$("#rexona").off('mouseleave');
